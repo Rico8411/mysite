0dce773d6e11c080bfe01113fe004ad969847fac
Rico8411 <lm407479@gmail.com>
2025-09-02 17:21:56 +0330

    create about page and contact page and element page and create base.html

--- website/views.py
+++ website/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'index.html')
+    return render(request,'website/index.html')
 
+def about(request):
+    return render(request,'website/about.html')
+
+def contact(request):
+    return render(request,'website/contact.html')
+
+def elements(request):
+    return render(request,'website/elements.html')
 # Create your views here.
