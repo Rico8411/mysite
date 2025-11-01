bd0ea3a4088ff425658955a7845fe8b82a8838c2
ali mokhtari <lm407479@gmail.com>
2025-11-01 20:15:50 +0330

    Add contact form functionality with CSRF protection, and include new feature images for the blog

--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from django.shortcuts import redirect
 
 def index(request):
     return render(request,'website/index.html')
@@ -7,7 +9,14 @@ def about(request):
     return render(request,'website/about.html')
 
 def contact(request):
-    return render(request,'website/contact.html')
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('website:contact')
+    form = ContactForm()
+    return render(request,'website/contact.html', {'form': form})
 
 def elements(request):
     return render(request,'website/elements.html')
