c1698c53507fb186b28cf2da2a2f75041a49bc5e
ali mokhtari <lm407479@gmail.com>
2025-12-16 13:59:10 +0330

    Refactor blog templates to utilize inclusion tag for popular posts, enhancing code maintainability and dynamic content rendering. Update CSS for image responsiveness.

--- blog/templatetags/blog-tags.py
+++ blog/templatetags/blog-tags.py
@@ -1,3 +1,19 @@
 from django import template
-
+from blog.models import Post
 register = template.Library()
+
+# @register.simple_tag(name='posts')
+# def func():
+#     post = Post.objects.filter(status=1).count()
+
+
+# @register.filter
+# def value(value,args=100):
+#     return value[args]
+
+
+
+@register.inclusion_tag("blog/blog-popular-posts.html")
+def latestposts(args=3):
+    posts = Post.objects.filter(status=1).order_by('published_date')[:args]
+    return{'posts':posts}
--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', blog_view, name='index'),
     path('<int:pid>', blog_single, name='single'),
-
+    
 ]
