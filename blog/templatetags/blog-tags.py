87810d17c17af9dd21a5b2a19f4dce5d15bc6ae1
ali mokhtari <lm407479@gmail.com>
2025-12-16 14:20:02 +0330

    Implement post categories inclusion tag in blog templates, replacing static category listings for improved maintainability and dynamic content rendering.

--- blog/templatetags/blog-tags.py
+++ blog/templatetags/blog-tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, category
 register = template.Library()
 
 # @register.simple_tag(name='posts')
@@ -17,3 +17,14 @@ register = template.Library()
 def latestposts(args=3):
     posts = Post.objects.filter(status=1).order_by('published_date')[:args]
     return{'posts':posts}
+
+
+@register.inclusion_tag("blog/blog-post-categories.html")
+def post_categories():
+    posts = Post.objects.filter(status=1)
+    categories = category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+    return {'categories':cat_dict}
+        
