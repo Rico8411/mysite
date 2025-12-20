89aa5424033350243e2a52790f05d1effd555c54
ali mokhtari <lm407479@gmail.com>
2025-12-20 20:14:34 +0330

    Refactor blog views and URLs to consolidate category handling. Update blog-home template to link categories dynamically, enhancing navigation and maintainability.

--- blog/views.py
+++ blog/views.py
@@ -2,17 +2,19 @@ from django.shortcuts import render
 from blog.models import *
 from django.shortcuts import get_object_or_404
 
-def blog_view(request):
+def blog_view(request, cat_name=None):
     posts = Post.objects.filter(status=1)
+    if cat_name:
+        posts = posts.filter(category__name=cat_name)
     context = {'posts':posts}
     return render(request, 'blog/blog-home.html', context)
 
-def blog_single(request,pid):
+def blog_single(request, pid):
     post = get_object_or_404(Post, pk=pid, status=1)
     context = {'post':post}
     return render(request, 'blog/blog-single.html', context)
 
-def blog_category(request,cat_name):
+# def blog_category(request,cat_name):
     posts = Post.objects.filter(status=1)
     posts = posts.filter(category__name=cat_name)
     context = {'posts':posts}
