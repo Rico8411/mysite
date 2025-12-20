74770ac983f95ce4f4ee92aa7d043e6971f532cf
ali mokhtari <lm407479@gmail.com>
2025-12-20 20:00:37 +0330

    Add category view and update URL patterns for blog. Enhance blog-home template to link categories dynamically.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from blog.models import Post
+from blog.models import *
 from django.shortcuts import get_object_or_404
 
 def blog_view(request):
@@ -11,3 +11,9 @@ def blog_single(request,pid):
     post = get_object_or_404(Post, pk=pid, status=1)
     context = {'post':post}
     return render(request, 'blog/blog-single.html', context)
+
+def blog_category(request,cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {'posts':posts}
+    return render(request, 'blog/blog-home.html', context)
