494b5801f6315d1d0bfb339269cd901fdeed99db
ali mokhtari <lm407479@gmail.com>
2025-12-29 16:06:05 +0330

    Implement pagination in blog view and update blog-home template for dynamic page navigation, enhancing user experience. Add Google Maps iframe to contact page for improved location display.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from blog.models import *
+from blog.models import (Post, category)
 from django.shortcuts import get_object_or_404
+from django.core.paginator import( Paginator, EmptyPage, PageNotAnInteger)
+
 
 def blog_view(request, cat_name=None, author_name=None):
     posts = Post.objects.filter(status=1)
@@ -9,6 +11,16 @@ def blog_view(request, cat_name=None, author_name=None):
         posts = posts.filter(category__name=cat_name)
     if author_name:
         posts = posts.filter(author__username=author_name)
+
+    posts = Paginator(posts, 3)
+    page_number = request.GET.get('page')
+
+    try:
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
     context = {'posts':posts, 'categories':categories}
     return render(request, 'blog/blog-home.html', context)
 
