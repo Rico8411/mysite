3668c5ecd2622764e501c151b0692a94a3423411
ali mokhtari <lm407479@gmail.com>
2026-01-01 01:32:12 +0330

    Add Django sites and sitemaps integration in settings and URLs
    - Added 'django.contrib.sites' and 'django.contrib.sitemaps' to INSTALLED_APPS.
    - Configured SITE_ID for site management.
    - Implemented sitemap functionality in URL patterns, linking to static and blog sitemaps for improved SEO and content organization.

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,10 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 
     'website.apps.WebsiteConfig',
     'blog.apps.BlogConfig',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
