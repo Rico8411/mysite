8af3b26b9479937e89c3048d9baac3a896a1a350
ali mokhtari <lm407479@gmail.com>
2026-01-03 19:31:03 +0330

    Add Comment model and CommentAdmin for enhanced post interaction
    - Introduced Comment model with fields for name, email, subject, message, approval status, and timestamps.
    - Added CommentAdmin class to manage comments in the Django admin interface, including list display and filtering options.
    - Registered Comment model in the admin site for improved content management.

--- blog/admin.py
+++ blog/admin.py
@@ -11,6 +11,17 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('status','published_date')
     ordering = ('-created_date',)
     search_fields = ('title','content')
+
+
+class CommentAdmin(admin.ModelAdmin):
     
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('name','email','subject','approved','created_date')
+    list_filter = ('approved','created_date','post')
+    ordering = ('-created_date',)
+    search_fields = ('name','email','subject','message','post')
+
 admin.site.register(category)
 admin.site.register(Post,PostAdmin)
+admin.site.register(Comment,CommentAdmin)
