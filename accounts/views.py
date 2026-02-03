4a9d7ca8885f9cae9dd322d46fe0d4e029ccc340
ali mokhtari <lm407479@gmail.com>
2026-02-03 19:56:34 +0330

    Implement login functionality and enhance login template
    - Added authentication logic in login_view to handle user login and redirection.
    - Updated login.html to include CSRF token and correct action URL for form submission.
    - Minor formatting fix in base.html for consistent user display in navigation.

--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
 
 
 
 
+# Create your views here.
 def signup_view(request):
-    
     return render(request, 'accounts/signup.html')
 
-    
 def login_view(request):
-    if request.user.is_authenticated:
-        msg = f'user is authenticated as {request.user.username}'
-    else:
-        msg = 'user is not authenticated'
-    context = {'msg': msg}
-    return render(request, 'accounts/login.html', context)
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return redirect('accounts:login')
+        
+
+    return render(request, 'accounts/login.html')
 
 def logout_view(request):
     pass
