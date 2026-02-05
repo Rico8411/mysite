4160f3bdf9f167eac35defacaae35bcc32cb20be
ali mokhtari <lm407479@gmail.com>
2026-02-05 21:03:41 +0330

    Implement signup functionality and enhance login view for user authentication
    - Added signup_view to handle user registration with UserCreationForm and redirect to the login page upon successful signup.
    - Updated login_view to redirect authenticated users to the homepage and handle login attempts for unauthenticated users.
    - Modified signup.html to include the correct action URL for form submission and added CSRF token for security.

--- accounts/views.py
+++ accounts/views.py
@@ -1,25 +1,28 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 
 
 
 # Create your views here.
-def signup_view(request):
-    return render(request, 'accounts/signup.html')
+
 
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('/')
-        else:
-            return redirect('accounts:login')
-        
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else:
+                return redirect('accounts:login')
+    else:
+        return redirect('/')
 
     return render(request, 'accounts/login.html')
 
@@ -27,3 +30,16 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('/')
+
+def signup_view(request):
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect(reverse('accounts:login'))
+        form = UserCreationForm()
+        context = {'form':form}
+        return render(request, 'accounts/signup.html', context)
+    else:
+        return redirect('/')
